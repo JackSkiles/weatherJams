6e5d92c01c9632bcb1c8f299064422e9b1a3c246
Jack Skiles <jackskiles420@gmail.com>
2020-06-30 12:19:30 -0400

    added spotify api functionality

--- src/App.js
+++ src/App.js
@@ -5,17 +5,38 @@ import Weather from "./components/Weather";
 import SongList from "./components/SongList"
 import {Link} from "react-router-dom" 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Spotify from 'spotify-web-api-js'
+
+const spotifyWebApi = new Spotify();
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+    const params = this.getHashParams()
     this.state = {
       weather: "",
-      background: "url(./Clear.gif)"
+      background: "url(./Clear.gif)",
+      loggedInt: params.access_token ? true : false,
+      nowPlaying: {
+        name: 'Not Checked',
+        image: ''
+      }
+    }
+    if (params.access_token){
+      spotifyWebApi.setAccessToken(params.access_token)
     }
   }
 
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+  
   weatherGet = (state) => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${state.city},${state.state},${state.country}&appid=c6009635b8482d222193df4508ba690a`)
       .then(res => res.json())
@@ -42,6 +63,19 @@ class App extends React.Component {
     }
   }
 
+  getNowPlaying(){
+    spotifyWebApi.getMyCurrentPlaybackState()
+    .then((response) => {
+      console.log("Hello")
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          image: response.item.album.images[0].url
+        }
+      })
+    })
+  }
+
   render() {
     return (
       <Router>
@@ -58,7 +92,16 @@ class App extends React.Component {
                 <Weather handleSubmit={(e, state) => {
                   this.weatherGet(state)
                 }} />
-                <button onClick={this.getSong}>Get Songs</button>
+              <a href='http://localhost:8888'>
+                <button>Login With Spotify</button>
+              </a>
+              <div> Now Playing: {this.state.nowPlaying.name }</div>
+              <div>
+                <img src={ this.state.nowPlaying.image } style={{width: 100}}/>
+              </div>
+              <button onClick={() => this.getNowPlaying()}>
+                Check Now Playing
+              </button>
               </div>
             </Route>
           </Switch>
